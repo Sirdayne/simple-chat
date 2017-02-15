f2a85d78b3f0e8fdac09f582d92cff8ea41e3178
Dayne <mussinyernar@gmail.com>
2017-02-15 12:24:54 +0600

    mockup ended

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		console.log(th);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize(),
+
+		}).done(function() {
+            
+            $('.modal').fadeOut('fast');
+            $('.modal-calc').fadeOut('fast');
+            $('.modal-back').fadeIn('fast');
+            $('.thx').fadeIn('fast');
+            
+            //yaCounter39717215.reachGoal('send');
+            //ga('send', 'event', 'form', 'success');
+            
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
