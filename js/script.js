0d610f81d39058f4be4a60302dcfc4bb2999f4dd
Dayne <mussinyernar@gmail.com>
2017-02-15 18:08:30 +0600

    added first step

--- js/script.js
+++ js/script.js
@@ -1,30 +1,17 @@
+"use strict"
+
 $(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		console.log(th);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize(),
-
-		}).done(function() {
-            
-            $('.modal').fadeOut('fast');
-            $('.modal-calc').fadeOut('fast');
-            $('.modal-back').fadeIn('fast');
-            $('.thx').fadeIn('fast');
-            
-            //yaCounter39717215.reachGoal('send');
-            //ga('send', 'event', 'form', 'success');
-            
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+    
+    $('.user').click(function() {
+        
+        var indexUser = $('.user').index(this);
+        $('.user').removeClass('user-active');
+        $(this).addClass('user-active');
+        $('.chat-message').hide();
+        $('.chat-message').fadeIn(500);
+        $('#js-textarea').hide();
+        $('#js-textarea').fadeIn(500);
+        
+    });
 
 });
