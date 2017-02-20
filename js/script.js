0042f445f21a7be3f2ee8ab38dc6c840a114ca46
Dayne <mussinyernar@gmail.com>
2017-02-20 11:12:51 +0600

    refactored variables

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 "use strict"
 
 $(document).ready(function() {
-    $('#textarea').fadeOut(10).prop("disabled", false);
+    
+    
+    var textarea = $('#textarea');
+    var body = $('body');
+    
+    var chatMessages = $('.chat-messages');
+    var chatMessage = $('.chat-message');
+    
+    var user = $('.user');
+    var userTypes = $('.user-types');
+    var userNewMsg = $('.user-new-msg');
+    
+    var loading = $('.loading');
     
     var indexUser = 0;
+    
+    textarea.fadeOut(10).prop("disabled", false);
+    
     
     function Chat() {
         
@@ -14,7 +29,7 @@ $(document).ready(function() {
             companionTypes(indexUser);
             
             window.setTimeout(function(){
-                $('.chat-messages').append(`
+                chatMessages.append(`
                     <div class="chat-message" style="display:block;">
                         <div class="message-txt message-companion">`+"Привет, как дела?"+`
                             <div class="message-time">
@@ -26,28 +41,26 @@ $(document).ready(function() {
                 
                 companionTypesEnded(indexUser);
                 scrollBottomChat(500);
-                notification(indexUser);
+                notificationUser(indexUser);
                 
             }, 1500);
         }
         
         function scrollBottomChat(timeToScroll) {
-            $('.chat-messages').animate({ scrollTop: $('.chat-messages').prop('scrollHeight')}, timeToScroll);
+            chatMessages.animate({ scrollTop: chatMessages.prop('scrollHeight')}, timeToScroll);
         }
         
         function companionTypes(indexCompanion) {
-            $('.user').eq(indexCompanion).addClass('types-active');
+            user.eq(indexCompanion).addClass('types-active');
         }
         
         function companionTypesEnded(indexCompanion) {
-            $('.user').eq(indexCompanion).removeClass('types-active');
+            user.eq(indexCompanion).removeClass('types-active');
         }
         
-        function notification(indexCompanion) {
-            $('.user').eq(indexCompanion).addClass('user-got-msg');
-            var urlAvatar = $('.user').eq(indexCompanion).find('img').attr('src');
-            
-              Notification.requestPermission(function(permission){ 
+        function notification() {
+            /*
+            Notification.requestPermission(function(permission){ 
                 var notification=new Notification('Simple chat',{
                   body:'New message',
                   icon: urlAvatar
@@ -55,31 +68,40 @@ $(document).ready(function() {
                 setTimeout(function(){
                   notification.close();
                 },2000);
-              });
+            });
+            */
+        }
+        
+        function notificationUser(indexCompanion) {
+            user.eq(indexCompanion).addClass('user-got-msg');
+            var urlAvatar = user.eq(indexCompanion).find('img').attr('src');
+            
+            notification();
+            
         };
 
         this.userClicked = function(indexUserClicked) {
-            $('.chat-message').hide();
+            chatMessage.hide();
           
-            $('.user').removeClass('user-active');
-            $('.user-new-msg').eq(indexUserClicked).fadeOut().html();
-            $('.user').eq(indexUserClicked).addClass('user-active');
+            user.removeClass('user-active');
+            userNewMsg.eq(indexUserClicked).fadeOut().html();
+            user.eq(indexUserClicked).addClass('user-active');
 
-            $('.loading').addClass('active');
+            loading.addClass('active');
 
             window.setTimeout(function() {
-                $('.chat-message').fadeIn(500);
-                $('#textarea').show();
-                $('.loading').removeClass('active');
+                chatMessage.fadeIn(500);
+                textarea.show();
+                loading.removeClass('active');
             }, 1200);
         };
         
         this.sendMessage = function(message) {
-                $('#textarea').val('');
+                textarea.val('');
                 if (message != ''){
-                    $('#textarea').prop("disabled", true);
+                    textarea.prop("disabled", true);
                     window.setTimeout(function(){
-                        $('.chat-messages').append(`
+                        chatMessages.append(`
                             <div class="chat-message" style="display:block;">
                                 <div class="message-txt">`+message+`
                                     <div class="message-time">
@@ -89,7 +111,7 @@ $(document).ready(function() {
                             </div>
                         `);
 
-                        $('#textarea').prop("disabled", false);
+                        textarea.prop("disabled", false);
                         companionAnswers();
                         scrollBottomChat(1000);
                         
@@ -98,48 +120,40 @@ $(document).ready(function() {
         };
         
         this.companionTypes = function(indexCompanion) {
-            $('.user').eq(indexCompanion).addClass('types-active');
+            user.eq(indexCompanion).addClass('types-active');
             
             var timer = 0;
                 window.setInterval(function(){
                         ++timer;
                         var result = timer % 4;
-                            if (result == 0){$('.user-types').html('Собеседник печатает')}
-                            if (result == 1){$('.user-types').html('Собеседник печатает.')}
-                            if (result == 2){$('.user-types').html('Собеседник печатает..')}
-                            if (result == 3){$('.user-types').html('Собеседник печатает...')}
+                            if (result == 0){userTypes.html('Собеседник печатает')}
+                            if (result == 1){userTypes.html('Собеседник печатает.')}
+                            if (result == 2){userTypes.html('Собеседник печатает..')}
+                            if (result == 3){userTypes.html('Собеседник печатает...')}
                 }, 300);
         };
         
         this.companionSended = function(indexCompanion) {
-            $('.user').eq(indexCompanion).addClass('user-got-msg');
-            var urlAvatar = $('.user').eq(indexCompanion).find('img').attr('src');
+            user.eq(indexCompanion).addClass('user-got-msg');
+            var urlAvatar = user.eq(indexCompanion).find('img').attr('src');
             
-              Notification.requestPermission(function(permission){ 
-                var notification=new Notification('Simple chat',{
-                  body:'New message',
-                  icon: urlAvatar
-                });
-                setTimeout(function(){
-                  notification.close();
-                },2000);
-              });
+              notification();
         };
     }
     
     var chat = new Chat();
     
     /* ВЫБРАТЬ СОБЕСЕДНИКА */
-    $('.user').click(function() {
-        indexUser = $('.user').index(this);
+    user.click(function() {
+        indexUser = user.index(this);
         chat.userClicked(indexUser);
     });
     
     /* ОТПРАВКА СООБЩЕНИЯ */
-    $('body').on('focus','#textarea',function(){
-        $('#textarea').keydown(function(e) {
+    body.on('focus', '#textarea', function(){
+        textarea.keydown(function(e) {
             if (e.ctrlKey && e.keyCode == 13) {
-                var textareaVal = $('#textarea').val();
+                var textareaVal = textarea.val();
                 chat.sendMessage(textareaVal);
             }
         });
